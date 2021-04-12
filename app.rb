85f0616d320704532e29396012d623eb1303a91e
cynthia.anyaeriuba <canyaeriuba@gmail.com>
2021-04-12 17:43:49 +0100

    added views directory and moved html into index.erb file

--- app.rb
+++ app.rb
@@ -1,17 +1,7 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
 
-get '/' do
-  'hello'
-end
-
-get '/secret' do
-  'secret webpage'
-end
-
 get '/cat' do
-  "<div>
-    <img src='https://i.imgur.com/jFaSxym.png' style= 'border: 10px dashed red'>
-  </div>"
+  erb(:index)
 end
   
